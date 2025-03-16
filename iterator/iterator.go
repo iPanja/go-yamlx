f264adef2ad5262cea6ef172243fcb249e6ca57e
Fletcher Henneman <fhenneman@comcast.net>
2025-03-16 13:23:21 -0400

    WIP 2

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -1,6 +1,7 @@
 package iterator
 
 import (
+	"github.com/iPanja/go-yamlx/utils"
 	"gopkg.in/yaml.v3"
 )
 
@@ -17,11 +18,8 @@ func FromNode(node *yaml.Node) Iterator {
 }
 
 // FromNodes creates an Iterator on all given nodes
-// without modifying the actual nodes.
 func FromNodes(nodes ...*yaml.Node) Iterator {
 	index := 0
-	//var mergeNodes []*yaml.Node
-	//var mergeIndex int
 
 	return func() (node *yaml.Node, ok bool) {
 		// We have reached the end
@@ -37,6 +35,7 @@ func FromNodes(nodes ...*yaml.Node) Iterator {
 	}
 }
 
+// ResolveAliases will replace any Alias node with its underlying node
 func (iter Iterator) ResolveAliases() Iterator {
 	return func() (node *yaml.Node, ok bool) {
 		node, ok = iter()
@@ -52,63 +51,102 @@ func (iter Iterator) ResolveAliases() Iterator {
 	}
 }
 
-func (iter Iterator) EmbedMerges() Iterator {
-	var mergeIter Iterator
+// EmbedMerges will embed the aliases (or list of) and ensure subsequent merge blocks and aliases are resolved
+//func (iter Iterator) EmbedMerges() Iterator {
+//	return func() (node *yaml.Node, ok bool) {
+//		return lazyNodeProcess(iter)
+//	}
+//}
 
+type MergeState struct {
+	InMergeBlock bool
+	Aliases      []*yaml.Node
+}
+
+// EmbedMergesInMaps will update the contents of any maps that end up being iterated over.
+//
+// Their contents will reflect the final result of applying the merge key (both a single alias or a sequence are supported) if one is found
+func (iter Iterator) EmbedMergesInMaps() Iterator {
 	return func() (node *yaml.Node, ok bool) {
-		// Check if we are currently iterating through a merge block's contents
-		if mergeIter != nil {
-			node, ok = mergeIter()
-			if ok {
-				return
-			}
-
-			mergeIter = nil
-		}
-
-		// Normal iteration
 		node, ok = iter()
 		if !ok {
-			return
+			return nil, false
 		}
 
-		// Instead return first node of merge sequence, and use it for the future
-		// Key: Scalar Node (Tag == !!merge)
-		// Value: Alias (TODO: could be a sequence of aliases?)
-		if node.Tag == "!!merge" {
-			alias, aok := iter()
-			if !aok {
-				return // Yikes!
-			}
-
-			mergeIter = FromNodes(alias.Alias.Content...)
-
-			node, ok = mergeIter()
-			if !ok {
-				node, ok = iter() // TODO: see if this is correct behavior...
-			}
+		// Embeds only work within mapping nodes
+		if node.Kind != yaml.MappingNode {
+			return node, true
 		}
 
-		return
+		var mergeState MergeState
+		processMappingNode(node, &mergeState)
+
+		// If we're in a merge block, return a new node with merged content
+		// We do not want to modify the true mapping node, but instead return a duplicate, fully rendered, node
+		if mergeState.InMergeBlock {
+			mergedNode := &yaml.Node{
+				Kind:    node.Kind,
+				Content: append([]*yaml.Node{}, node.Content...), // Copy original
+			}
+
+			// Merge alias nodes into the new node
+			for _, alias := range mergeState.Aliases {
+				utils.MergeNodes(mergedNode, alias) // Follows YAML embedding precedence
+			}
+
+			return mergedNode, true
+		}
+
+		// Otherwise, return the original node
+		return node, true
 	}
 }
 
-// RecurseNodes recursively expands out on all children (sequence, mapping)
+// processMappingNode will extract the mergeState if found within the given map
+func processMappingNode(node *yaml.Node, mergeState *MergeState) {
+	// Resolve aliases in the node itself
+	//if node.Kind == yaml.AliasNode {
+	//	node = node.Alias
+	//}
+
+	// Process the node's content to handle merge keys
+	for i := 0; i < len(node.Content); i += 2 {
+		key := node.Content[i]
+		if key.Value == "<<" {
+			mergeValue := node.Content[i+1]
+			if mergeValue.Kind == yaml.AliasNode {
+				// <<: *alias
+				mergeState.InMergeBlock = true
+				mergeState.Aliases = append(mergeState.Aliases, mergeValue.Alias)
+			} else if mergeValue.Kind == yaml.SequenceNode {
+				// <<: [*alias_one, *alias_two, ...]
+				for _, aliasNode := range mergeValue.Content {
+					if aliasNode.Kind == yaml.AliasNode {
+						mergeState.InMergeBlock = true
+						mergeState.Aliases = append(mergeState.Aliases, aliasNode.Alias)
+					}
+				}
+			}
+
+			// Remove the merge key and its value (don't modify the original node)
+			node.Content = append(node.Content[:i], node.Content[i+2:]...)
+			i -= 2 // Adjust the index after removal
+		}
+	}
+}
+
+// RecurseNodes recursively traverse the nodes' children/content via BFS
 //
-// # Searches DFS style
-//
-// - Resolves aliases
-//
-// - Embeds merge blocks
+// All aliases will be resolved and all merge keys will be embedded
 func (iter Iterator) RecurseNodes() Iterator {
-	var stack []*yaml.Node
+	var queue []*yaml.Node
 
 	return func() (node *yaml.Node, ok bool) {
 		// Check stack
-		if len(stack) > 0 {
+		if len(queue) > 0 {
 			// node, ok = FromNode(stack[len(stack)-1])()
-			node = stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
+			node = queue[0]
+			queue = queue[1:]
 			ok = true
 		} else {
 			node, ok = iter()
@@ -117,14 +155,43 @@ func (iter Iterator) RecurseNodes() Iterator {
 			}
 		}
 
+		// Process node - resolve aliases and embed merge keys
+		node = processNode(node)
+
 		// Dive into children
 		if len(node.Content) > 0 {
-			childIter := FromNodes(node.Content...)
+			childIter := FromNodes(node.Content...).ResolveAliases().EmbedMergesInMaps()
+
 			for child, ok := childIter(); ok; child, ok = childIter() {
-				stack = append([]*yaml.Node{child}, stack...)
+				queue = append(queue, child)
 			}
 		}
 
 		return
 	}
+}
+
+func processNode(node *yaml.Node) *yaml.Node {
+	// Resolve aliases for the current node
+	if node.Kind == yaml.AliasNode {
+		node = node.Alias
+	}
+
+	// Embed merge keys if the node is a mapping node
+	if node.Kind == yaml.MappingNode {
+		var mergeState MergeState
+		processMappingNode(node, &mergeState)
+		if mergeState.InMergeBlock {
+			mergedNode := &yaml.Node{
+				Kind:    node.Kind,
+				Content: append([]*yaml.Node{}, node.Content...), // Copy original content
+			}
+			for _, alias := range mergeState.Aliases {
+				utils.MergeNodes(mergedNode, alias)
+			}
+			node = mergedNode
+		}
+	}
+
+	return node
 }
--- utils/utils.go
+++ utils/utils.go
@@ -22,3 +22,35 @@ func ParseYaml(yamlText string) *yaml.Node {
 
 	return &Node
 }
+
+// MergeNodes will merge src -> dst while following proper YAML merge behavior
+//
+// This function will modify dst. Use a duplicate, dummy, dst node if needed.
+func MergeNodes(dst, src *yaml.Node) {
+	if dst.Kind != yaml.MappingNode || src.Kind != yaml.MappingNode {
+		return // Only merge mapping nodes
+	}
+
+	// Create a map of keys in the destination node
+	keyMap := make(map[string]int)
+	for i := 0; i < len(dst.Content); i += 2 {
+		key := dst.Content[i].Value
+		keyMap[key] = i + 1 // Store the index of the value
+	}
+
+	// Merge keys from the source node into the destination node
+	for i := 0; i < len(src.Content); i += 2 {
+		key := src.Content[i].Value
+		if key == "<<" {
+			continue // Skip merge keys as we handled them
+		}
+
+		// If the key already exists in the destination, skip it
+		// Keys in mapping nodes earlier in the sequence override keys specified in later mapping nodes
+		if _, exists := keyMap[key]; exists {
+			continue
+		}
+
+		dst.Content = append(dst.Content, src.Content[i], src.Content[i+1])
+	}
+}
